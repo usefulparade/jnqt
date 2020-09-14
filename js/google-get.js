7ef5d1335e689595b213f34de43b17f3b27bbb5a
unknown <jessequebbeman@gmail.com>
2020-09-14 16:00:32 -0700

    change in header and subheader

--- js/google-get.js
+++ js/google-get.js
@@ -35,8 +35,8 @@ function populateContent(sheet){
     firstTransition.parent(archiveBlock);
 
     titleBlock = createDiv().addClass('pull-left main-title');
-    titleH1 = createElement('h1', '~jnqt~');
-    titleH2 = createElement('h2', "It's me!");
+    titleH1 = createElement('h1', 'jnqt');
+    titleH2 = createElement('h2', "my friends, music, etc.");
     titleH1.parent(titleBlock);
     titleH2.parent(titleBlock);
     titleBlock.parent(archiveBlock);
