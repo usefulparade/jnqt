e5e33bf96a53dad71f8e7917e47cfe97a3516d7a
unknown <jessequebbeman@gmail.com>
2020-09-14 12:27:06 -0700

    jesse is now a collaborator

--- js/google-get.js
+++ js/google-get.js
@@ -35,8 +35,8 @@ function populateContent(sheet){
     firstTransition.parent(archiveBlock);
 
     titleBlock = createDiv().addClass('pull-left main-title');
-    titleH1 = createElement('h1', '~JNQT~');
-    titleH2 = createElement('h2', "Hi! I'm Jesse, I play drums and write music!<br>This is an archive and portfolio of those things.");
+    titleH1 = createElement('h1', '~jnqt~');
+    titleH2 = createElement('h2', "It's me!");
     titleH1.parent(titleBlock);
     titleH2.parent(titleBlock);
     titleBlock.parent(archiveBlock);
