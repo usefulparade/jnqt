b1c1a125f48742419be4b6f8c6625f4d875d931d
jnqt <jessequebbeman@gmail.com>
2025-05-13 12:44:03 -0700

    Update csv-get.js
    remote email from headers

--- js/csv-get.js
+++ js/csv-get.js
@@ -53,11 +53,9 @@ function populateContent(sheet){
     titleBlock = createDiv().addClass('pull-left main-title');
     titleH1 = createElement('h1', 'JNQT');
     titleH2 = createElement('h2', "an incomplete archive of Jesse Quebbeman-Turley's recordings and performances");
-    titleH3 = createElement('h3', "jessequebbeman@gmail.com");
 
     titleH1.parent(titleBlock);
     titleH2.parent(titleBlock);
-    titleH3.parent(titleBlock);
     titleBlock.parent(archiveBlock);
 
     secondTransition = createDiv().addClass('transition-l-to-r');
